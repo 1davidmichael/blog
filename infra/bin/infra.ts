3465ead155e46690016912a0e724bc464577f953
David Michael <david.a.michael@emcins.com>
2020-07-08 19:37:24 -0500

    Add aws account info to cdk

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -2,6 +2,15 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { InfraStack } from '../lib/infra-stack';
+import { countReset } from 'console';
+import { Tag } from '@aws-cdk/core';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack');
+const stack = new InfraStack(app, 'InfraStack', {
+    env: {
+        'account': process.env.AWS_ACCOUNT_ID,
+        'region': process.env.AWS_DEFAULT_REGION
+    }
+});
+
+Tag.add(stack, "Environment", "Prod");
